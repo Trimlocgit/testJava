879c3f1840885e1bc71d8a5967a5a41515c57058
FORTIN Victor <victor.fortin078@gmail.com>
2025-03-24 20:03:17 +0100

    first java commit

---
+++ Client.java
@@ -0,0 +1,8 @@
+public class Client {
+    public static void main(String[] args) {
+        //String jaja = "Coucou";
+        String jojo = "Lysandre le skibidi metaphoreeeeee rephatazioooooo";
+        String jiji = "Cyprien le plus grand guerrrier de l'Akatsuki";
+        System.out.println(jojo.compareTo(jiji));
+    }
+}
